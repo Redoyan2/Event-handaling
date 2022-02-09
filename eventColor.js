fa1b1e6693b9b9c2f581160becf8ed472df6bf2d
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-09 13:06:55 +0600

    event handeling

---
+++ eventColor.js
@@ -0,0 +1,17 @@
+
+function makeYellow(){
+    document.body.style.backgroundColor='yellow';
+}
+const greenButton = document.getElementById('make-button-green');
+
+greenButton.onclick = makeGreen;
+
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+
+
+const blueButton = document.getElementById('make-button-blue').onclick=function(){
+    document.body.style.backgroundColor='blue';
+}
